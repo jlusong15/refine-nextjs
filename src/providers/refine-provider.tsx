4155789b34831c65c93b24b18077dc044ad662b1
Mochi <jennalilubautista@gmail.com>
2026-07-02 11:24:59 +0800

    Add applicants on refine provider

--- src/providers/refine-provider.tsx
+++ src/providers/refine-provider.tsx
@@ -9,7 +9,19 @@ const API_URL = process.env.NEXT_PUBLIC_STRAPI_URL!
 
 export default function RefineProvider({ children }: { children: React.ReactNode }) {
 	return (
-		<Refine routerProvider={routerProvider} dataProvider={DataProvider(API_URL, axiosInstance)} resources={[]}>
+		<Refine
+			routerProvider={routerProvider}
+			dataProvider={DataProvider(API_URL, axiosInstance)}
+			resources={[
+				{
+					name: "applicants",
+					list: "/applicants",
+					create: "/applicants/create",
+					edit: "/applicants/edit/:id",
+					show: "/applicants/:id",
+				},
+			]}
+		>
 			{children}
 		</Refine>
 	)
