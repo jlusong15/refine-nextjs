c0917be091419755f47823955bcc48b9a19fc273
Mochi <jennalilubautista@gmail.com>
2026-07-03 16:15:55 +0800

    Add Suspense and ErrorBoundary

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,20 @@
 "use client"
 
 import AppInitializer from "@/components/AppInitializer"
+import ErrorPage from "@/components/layout/ErrorPage"
+import Loading from "@/components/shared/Loading"
 import RefineProvider from "@/providers/refine.provider"
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
-		<RefineProvider>
-			<AppInitializer>{children}</AppInitializer>
-		</RefineProvider>
+		<Suspense fallback={<Loading />}>
+			<ErrorBoundary fallback={<ErrorPage />}>
+				<RefineProvider>
+					<AppInitializer>{children}</AppInitializer>
+				</RefineProvider>
+			</ErrorBoundary>
+		</Suspense>
 	)
 }
